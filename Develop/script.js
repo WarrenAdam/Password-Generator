5b2429ade66309023fc5a6fe47a6cee54a20ab05
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-05-16 11:17:49 -0400

    added confirm for password choices inside promt function.

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,8 @@ var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var special = ['@', '!', '#', '$', '&', '%', '*'];
 
+var length = 8;
+var passwordOptions = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -21,4 +23,26 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
+}
+
+function prompts() {
+  length = parseInt(prompt("How many characters (between 8-128) would you like for your password?"));
+
+  if (isNaN(length) || length < 8 || length >128) {
+    alert("Please try again, password length must be 8 - 128 digits.");
+    return false;
+  }
+  // decides what user would like in password
+if (confirm("Would you like lowercase letters in your password?")) {
+  passwordOptions = passwordOptions.concat(lowerCase);
+}
+if (confirm("Would you like uppercase letters in your password?")) {
+  passwordOptions = passwordOptions.concat(upperCase);
+}
+if (confirm("Would you like special characters in your password?")) {
+  passwordOptions = passwordOptions.concat(special);
+}
+if (confirm("Would you like numbers in your password?")) {
+  passwordOptions = passwordOptions.concat(numbers);
+}
 }
