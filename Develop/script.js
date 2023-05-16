e1986fe7776a448c5dbe80e71c64dd57d5754ef8
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-05-16 12:15:18 -0400

    added for loop to generate password function and fixed bug in write passwords function

--- Develop/script.js
+++ Develop/script.js
@@ -1,48 +1,63 @@
+
 var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var special = ['@', '!', '#', '$', '&', '%', '*'];
 
-var length = 8;
+var charLength = 8;
 var passwordOptions = [];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+function writePassword() {
+  var rightPrompts = prompts();
+  var passwordText = document.querySelector("#password");
+
+  if (rightPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+      passwordTest.value = "";
+  }
+
+}
+
 function generatePassword() {
+  var password = "";
+  for (var i = 0; i < charLength; i++) {
+    var randomLetter = Math.floor(Math.random() * passwordOptions.length);
+    password = password + passwordOptions[randomLetter];
+  }
+  return password;
 
 }
 
 function prompts() {
-  length = parseInt(prompt("How many characters (between 8-128) would you like for your password?"));
+  charLength = parseInt(prompt("How many characters (between 8-128) would you like for your password?"));
+  passwordOptions = [];
 
-  if (isNaN(length) || length < 8 || length >128) {
+  if (isNaN(charLength) || charLength < 8 || charLength > 128) {
     alert("Please try again, password length must be 8 - 128 digits.");
     return false;
   }
   // decides what user would like in password
-if (confirm("Would you like lowercase letters in your password?")) {
-  passwordOptions = passwordOptions.concat(lowerCase);
-}
-if (confirm("Would you like uppercase letters in your password?")) {
-  passwordOptions = passwordOptions.concat(upperCase);
-}
-if (confirm("Would you like special characters in your password?")) {
-  passwordOptions = passwordOptions.concat(special);
-}
-if (confirm("Would you like numbers in your password?")) {
-  passwordOptions = passwordOptions.concat(numbers);
-}
+  if (confirm("Would you like your password to have lowercase letters?")) {
+    passwordOptions = passwordOptions.concat(lowerCase);
+  }
+  if (confirm("Would you like your password to have uppercase letters?")) {
+    passwordOptions = passwordOptions.concat(upperCase);
+  }
+  if (confirm("Would you like your password to have special characters?")) {
+    passwordOptions = passwordOptions.concat(special);
+  }
+  if (confirm("Would you like your password to have numbers?")) {
+    passwordOptions = passwordOptions.concat(numbers);
+  }
+  return true;
 }
